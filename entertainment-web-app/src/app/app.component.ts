4a3941e7b496ad9fa460fd4172dc2ecad4fa9e0a
IT Compliance <rhodaarthur48@gmail.com>
2024-09-24 21:00:26 -0700

    Adds form layout

--- entertainment-web-app/src/app/app.component.ts
+++ entertainment-web-app/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MenuComponent} from "./components/menu/menu.component";
 import {SearchComponent} from "./components/search/search.component";
+import {LoginComponent} from "./components/form/login/login.component";
+import {SignupComponent} from "./components/form/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, MenuComponent, SearchComponent],
+    imports: [RouterOutlet, MenuComponent, SearchComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
